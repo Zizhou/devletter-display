83078c95211160b3ed12a5a0762525ecefceb331
Daniel <dan.t.ching@gmail.com>
2014-05-01 21:06:29 -0700

    user queue and sig change

--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required, permission_required
 
 from submit.models import Game, Developer
+from display.models import Letter, UserProfile
 # Create your views here.
 
 @login_required
@@ -40,11 +41,26 @@ def profile(request, dev_id):
     }
     return render(request, 'display/profile.html', context)
 
+#user profile page, moved here, since it's also doubling as the personal queue
+#kinda jank, to be honest
+@login_required
 def user_profile(request):
-    username = 'No-Man'
-    if request.user.is_authenticated():
-	username = username = request.user.username
+    username = request.user.username
+    #this is so gonna fail at some point...
+    user_profile = UserProfile.objects.filter(user_id = request.user.id)[0]
+    #...and if that doesn't, this surely will
+    if request.method == 'POST':
+        user_profile.signature = request.POST.get('sig_change')
+        user_profile.save()
+
+    sig = user_profile.signature
+
+    #what's it being ordered by? who knows!
+    queue = user_profile.devlist.all().order_by()
+
     context = {
-        'name' : username
+        'name' : username,
+        'sig' : sig,
+        'queue' : queue
     }
     return render(request, 'display/user_profile.html', context)
